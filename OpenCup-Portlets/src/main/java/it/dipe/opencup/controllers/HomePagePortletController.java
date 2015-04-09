30ba7bf4997522c7d8bd070106dac79a923928b3
marcoiezzi <marcoiezzi.work@gmail.com>
2015-04-09 17:59:47 +0200

    nuova grafica

--- OpenCup-Portlets/src/main/java/it/dipe/opencup/controllers/HomePagePortletController.java
+++ OpenCup-Portlets/src/main/java/it/dipe/opencup/controllers/HomePagePortletController.java
@@ -6,8 +6,10 @@ import javax.portlet.RenderResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.WebKeys;
 
 @Controller
 @RequestMapping("VIEW")
@@ -16,7 +18,9 @@ public class HomePagePortletController {
 	public String renderRequest(RenderRequest renderRequest, 
 								RenderResponse renderResponse,
 								Model model){
-		
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+		model.addAttribute("jsFolder",themeDisplay.getPathThemeJavaScript());
+	
 		return "homepage-view";
 	}
 }
